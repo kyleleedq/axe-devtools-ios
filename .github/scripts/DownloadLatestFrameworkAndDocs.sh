3435e664e37baeda67cff8c297724e5cd55ff1b6
Kyle Lee <kylelee@Kyles-MacBook-Pro.local>
2024-07-02 11:51:22 -0700

    Successfully download and unzip framework and docs

--- .github/scripts/DownloadLatestFrameworkAndDocs.sh
+++ .github/scripts/DownloadLatestFrameworkAndDocs.sh
@@ -13,10 +13,12 @@ LOCAL_ZIPPED_DOCS_URL="${OUTPUT_DIR}/${DOCS_FILENAME}.zip"
 
 mkdir -p $OUTPUT_DIR
 
-curl -H "X-JFrog-Art-Api:${DQ_AGORA_KEY}" \ 
-    -o $LOCAL_ZIPPED_FRAMEWORK_URL \ 
-    $FRAMEWORK_URL
-
+# Download & unzip framework
+curl -H "X-JFrog-Art-Api:${DQ_AGORA_KEY}" -o $LOCAL_ZIPPED_FRAMEWORK_URL $FRAMEWORK_URL
 unzip $LOCAL_ZIPPED_FRAMEWORK_URL -d $OUTPUT_DIR
+
+# Download & unzip docs
+curl -H "X-JFrog-Art-Api:${DQ_AGORA_KEY}" -o $LOCAL_ZIPPED_DOCS_URL $DOCS_URL
+unzip $LOCAL_ZIPPED_DOCS_URL -d $OUTPUT_DIR
 
 ls $OUTPUT_DIR
